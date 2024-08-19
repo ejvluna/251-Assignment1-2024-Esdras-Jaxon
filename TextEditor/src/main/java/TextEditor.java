32ad00d6157eb1899a8e85b103f7963c6ef0ca84
ejvluna <137990447+ejvluna@users.noreply.github.com>
2024-08-19 17:07:09 +1200

    First commit of the TextEditor Java class

---
+++ TextEditor/src/main/java/TextEditor.java
@@ -0,0 +1,9 @@
+// Section 0: Import the necessary libraries/packages
+
+import javax.swing.*; // to use Swing components
+import java.awt.*; // to use AWT components
+import java.awt.event.ActionEvent; // to handle action events
+import java.awt.event.ActionListener; // to listen for action events
+import java.io.*; // to read/write files
+
+
